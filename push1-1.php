bcc9a2944fde6d87654510efe0879106fafcf21c
Boatdk <boatz.dk@gmail.com>
2020-01-28 11:48:07 +0700

    edit layout

--- push1-1.php
+++ push1-1.php
@@ -2,7 +2,9 @@
 <html>
 
 <head>
-  <title></title>
+  <title>Reply Chat-Bot</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <style>
     body {
       background: #ccc;
@@ -31,21 +33,26 @@
 </head>
 
 <body>
-  <br>
-  <table align="center">
-    <tr align="center">
-      <td align="center">
-        <form action="" method="post" align="center">
-          <input type="text" name="msg"><br><br>
-          <input type="submit" name="SubmitButton">
-        </form>
-      </td>
-    </tr>
-
-  </table>
+  <div class="container">
+    <div>
+      <span> answer to <?php echo $id?> </span>
+    </div>
+    <form action="" method="post">
+      <div class="form-group">
+        <input type="text" name="msg"><br><br>
+        <input class="btn btn-primary" type="submit" name="SubmitButton">
+      </div>
+    </form>
+  </div>
 </body>
 
 </html>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+  integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+  integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+  integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <?php
 error_reporting(0);
 $id = $_REQUEST['uid'];
