90da9f8f6995c8c766eadc91d4011e0c5436e9f5
Boatdk <boatz.dk@gmail.com>
2020-01-28 16:39:37 +0700

    line

--- push1-1.php
+++ push1-1.php
@@ -18,27 +18,17 @@
     }
 
     .green {
-      background-color: #00b32d;
+      background-color: #f3f2f2;
     }
   </style>
 </head>
 
 <body>
   <nav class="navbar navbar-secondary green navbar-expand-sm">
-    <a class="navbar-brand" href="#">
-      <img src="http://shemagazine.net/wp-content/uploads/2018/02/line-logo.png" width="50" height="50" alt="">
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-      aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-        <a class="nav-item nav-link" href="#">Features</a>
-        <a class="nav-item nav-link" href="#">Pricing</a>
-        <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </div>
+    <div class="text-center">
+      <a class="navbar-brand" href="#">
+        <img src="http://shemagazine.net/wp-content/uploads/2018/02/line-logo.png" width="50" height="50" alt="">
+      </a>
     </div>
   </nav>
   <div class="container">
