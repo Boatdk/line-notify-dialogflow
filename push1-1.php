3209cf7a8ca8b6e8e9d08b1fefb4a6a35846278b
Boatdk <boatz.dk@gmail.com>
2020-01-28 14:44:00 +0700

    edit layout

--- push1-1.php
+++ push1-1.php
@@ -12,43 +12,18 @@
 
 <body>
   <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
-        aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
-    <div>
-      <span> answer to <?php echo $name?> </span>
-    </div>
-    <div class="text-center">
-      <form action="" method="post">
-        <div class="form-group">
-          <input type="text" name="msg"><br><br>
-          <input class="btn btn-primary" type="submit" name="SubmitButton">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="text-center">
+          <form action="" method="post">
+            <div class="form-group">
+              <input type="text" name="msg">
+            </div>
+            <input class="btn btn-primary" type="submit" name="SubmitButton">
+          </form>
         </div>
-      </form>
+      </div>
     </div>
-
   </div>
 </body>
 
